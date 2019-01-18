b84b37032987720c3f7d7d64c2ec7b1ee8e491f9
Sylwester Szczepaniak <sylwek_s@yahoo.com>
2019-01-18 14:33:41 +0100

    memory stubs added

--- src/main.c
+++ src/main.c
@@ -10,14 +10,10 @@
  *****************************************************************************/
 /**
  * @file main.c
- * @brief Main entry point to the C1M2 Assessment
+ * @brief Main entry for course 1 final assesment
  *
- * This file contains the main code for the C1M2 assesment. Students
- * are not to change any of the code, they are instead supposed to compile
- * these files with their makefile.
- *
- * @author Alex Fosdick
- * @date April 1 2017
+ * @author Sylwester Szczepaniak
+ * @date January 18 2019
  *
  */
 #include "platform.h"
@@ -27,28 +23,13 @@
 char buffer[MAX_LENGTH];
 
 /* A pretty boring main file */
-int main(void) {
-  unsigned int i;
-  char value;
+int main(void)
+{
 
-  /* Code below does some arbitrary memory Reads & writes */
-  clear_all(buffer, MAX_LENGTH);
-  set_all( ( buffer + 8 ), 43, 2); 
-  set_value(buffer, 0, 0x61);
-  value = get_value(buffer, 9);
-  set_value(buffer, 9, (value + 0x27));
-  set_value(buffer, 3, 0x37);
-  set_value(buffer, 1, 88);
-  set_value(buffer, 4, '2');
-  value = get_value(buffer, 1);
-  set_value(buffer, 2, 121);
-  set_value(buffer, 7, (value - 12));
-  set_value(buffer, 5, 0x5F);
+#ifdef COURSE1
+    course1();
+#endif
 
-  for ( i = 0; i < MAX_LENGTH; i++ ){
-    PRINTF("%c", buffer[i]);
-  }
-  PRINTF("\n");
   return 0;
 }
 
